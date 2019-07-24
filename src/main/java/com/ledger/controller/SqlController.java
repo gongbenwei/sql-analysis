0d052f62fca1d806cdcf220fbb0d35375133483b
gongbenwei <45281386+gongbenwei@users.noreply.github.com>
2019-07-24 16:17:28 +0800

    Update SqlController.java

--- src/main/java/com/ledger/controller/SqlController.java
+++ src/main/java/com/ledger/controller/SqlController.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -52,20 +51,27 @@ public class SqlController {
             //将{{.}}替换成？，然后在进行解析
             sql = sql.replaceAll(patternString, "?");
             SqlParser parser = new SqlParser();
-            Query query = (Query)parser.createStatement(sql);
-            QuerySpecification body = (QuerySpecification)query.getQueryBody();
-            SqlAnalysis sqlVo = new SqlAnalysis();
-            Select select = body.getSelect();
             List<String> columns = new ArrayList<>();
             List<String> tables = new ArrayList<>();
+            Query query = (Query)parser.createStatement(sql);
+            logger.info("queryBody: "+query.getQueryBody().getChildren());
+            SqlAnalysis sqlVo = new SqlAnalysis();
             Map<String,List<String>> tableColRelation = new HashMap<>();
-            Relation relation = body.getFrom().get();
-            //
-            List<Node> nodes = getAllNode(relation);
-            logger.info("nodes: "+nodes);
-            columns = getColumns(select,columns);
-            tables = traversalMany(nodes, tables);
-            tableColRelation = getTableColRelation(tables,getAllColumn(sql));
+            HiveSchemaStatVisitor visitor = getVisitor(sql);
+            tables = getAllTable(visitor);
+            if (query.getQueryBody().getChildren().get(0) instanceof Select){
+                QuerySpecification body = (QuerySpecification)query.getQueryBody();
+                Select select = body.getSelect();
+                columns = getColumns(select,columns);
+            }
+            if(query.getQueryBody().getChildren().get(0) instanceof QuerySpecification){
+                for (int i=0; i<query.getQueryBody().getChildren().size(); i++){
+                    QuerySpecification body = (QuerySpecification)query.getQueryBody().getChildren().get(i);
+                    Select select = body.getSelect();
+                    columns = getColumns(select,columns);
+                }
+            }
+            tableColRelation = getTableColRelation(tables,getAllColumn(visitor));
             sqlVo.setColumns(columns);
             sqlVo.setTables(tables);
             sqlVo.setWheres(wheres);
@@ -83,70 +89,12 @@ public class SqlController {
     //解析拿到列
     public List<String> getColumns(Select select, List<String> columnsList){
         for (SelectItem item: select.getSelectItems()){
+            if (columnsList.contains(item.toString())){
+                continue;
+            }
             columnsList.add(item.toString());
         }
         return columnsList;
-    }
-
-    //解析树，得到表名
-    public List<String> traversal(Node node, List<String> tableList){
-        if (node instanceof Table) {
-            Table table = (Table)node;
-            tableList.add(table.getName().toString());
-
-        }
-        if (node instanceof  AliasedRelation){
-            AliasedRelation aliasedRelation = (AliasedRelation)node;
-            Table table = (Table)aliasedRelation.getRelation();
-            //Identifier identifier = aliasedRelation.getAlias();
-            tableList.add(table.getName().toString());
-        }
-        return tableList;
-    }
-
-    //解析拿到所有的底层node
-    public List<Node> getAllNode (Node node){
-        List<Node> nodes = new ArrayList<>();
-        if (node instanceof Join){
-            Join join = (Join)node;
-            Node leftNode = join.getLeft();
-            Node rightNode = join.getRight();
-            List<Node> arr;
-            if (!(leftNode instanceof Join)){
-                nodes.add(leftNode);
-            }else{
-                arr = getAllNode(leftNode);
-                nodes.addAll(arr);
-            }
-
-            if (!(rightNode instanceof Join)){
-                nodes.add(rightNode);
-            }else{
-                arr = getAllNode(rightNode);
-                nodes.addAll(arr);
-            }
-        }else{
-            nodes.add(node);
-        }
-        return nodes;
-    }
-
-    //解析树，得到表名，支持多node
-    public List<String> traversalMany(List<Node> nodes, List<String> tableList){
-        for (Node node : nodes){
-            if (node instanceof Table) {
-                Table table = (Table)node;
-                tableList.add(table.getName().toString());
-
-            }
-            if (node instanceof  AliasedRelation){
-                AliasedRelation aliasedRelation = (AliasedRelation)node;
-                Table table = (Table)aliasedRelation.getRelation();
-                Identifier identifier = aliasedRelation.getAlias();
-                tableList.add(table.getName().toString()+" "+identifier.getValue());
-            }
-        }
-        return tableList;
     }
 
     //根据sql语句，拿到table与字段的对应关系，要求sql写法满足table.colName
@@ -170,19 +118,35 @@ public class SqlController {
         return tabColRelationMAp;
     }
 
-    //利用alibaba-druid得到所有的列，函数也支持，要求SQL的列的写法是表名.列名
-    public List<String> getAllColumn(String sql){
-        List<String> allComluns = new ArrayList<>();
+    //利用alibaba-druid获取得到druid的visitor对象
+    public HiveSchemaStatVisitor getVisitor(String sql){
         SQLStatementParser parser = new HiveStatementParser(sql);
         SQLStatement statement = parser.parseStatement();
         HiveSchemaStatVisitor visitor = new HiveSchemaStatVisitor();
         statement.accept(visitor);
+        return visitor;
+    }
+
+    //从visitor对象中得到所有的表
+    static List<String> getAllTable(HiveSchemaStatVisitor visitor){
+        List<String> allTables = new ArrayList<>();
+        HashMap<TableStat.Name, TableStat> tables = (HashMap<TableStat.Name, TableStat>) visitor.getTables();
+        for (TableStat.Name tabName : tables.keySet()){
+            allTables.add(tabName.toString());
+        }
+        logger.info("alibaba-druid get allTabs: "+allTables);
+        return allTables;
+    }
+
+    //从visitor对象中得到所有的列，函数也支持，要求SQL的列的写法是表名.列名
+    public List<String> getAllColumn(HiveSchemaStatVisitor visitor){
+        List<String> allColumns = new ArrayList<>();
         Collection<TableStat.Column> columns = visitor.getColumns();
         Iterator it=columns.iterator();
         while (it.hasNext()){
-            allComluns.add(it.next().toString());
+            allColumns.add(it.next().toString());
         }
-        logger.info("alibaba-druid get col: "+allComluns);
-        return allComluns;
+        logger.info("alibaba-druid get col: "+allColumns);
+        return allColumns;
     }
 }
